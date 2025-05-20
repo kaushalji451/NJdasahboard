117dcedd8840e732ab651017504a64a26d16674d
abhishek kaushal <abhishekkaushal2526@gmail.com>
2025-05-20 21:12:18 +0530

    added tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
+import AddCandidate from './pages/AddCandidate'
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Dashboard />} />
+        <Route path='/add' element={<AddCandidate />} />
       </Routes>
     </div>
   )
---
+++ frontend/src/pages/AddCandidate.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const AddCandidate = () => {
+  return (
+    <>
+      <div>
+        <h1 className="">Add A New Candidate</h1>
+        <form>
+          <label htmlFor=""></label>
+          <input type="text" />
+
+          <label htmlFor=""></label>
+          <input type="text" />
+
+          <label htmlFor=""></label>
+          <input type="text" />
+
+          <label htmlFor=""></label>
+          <input type="text" />
+
+          <label htmlFor=""></label>
+          <input type="text" />
+
+          <label htmlFor=""></label>
+          <input type="text" />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AddCandidate;
