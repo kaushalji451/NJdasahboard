5c858e4ea8a56135cd2a58846b7f656df0d87e22
abhishek kaushal <abhishekkaushal2526@gmail.com>
2025-05-24 18:15:22 +0530

    home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import AddCandidate from './pages/AddCandidate'
+import Home from './pages/Home'
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/add' element={<AddCandidate />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </div>
   )
