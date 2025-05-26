732c5809eeb70662fab4d37350e4e2263bbd6bdd
abhishek kaushal <abhishekkaushal2526@gmail.com>
2025-05-26 21:53:51 +0530

    home done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const Home = () => {
-  return <div>this is home page</div>;
+  const navigate = useNavigate();
+  const handleClick = (role) => {
+    if(role === "Candidate") {
+      navigate("/signup");
+    }else{
+      // navigate("/admin");
+    }
+  };
+  return (
+    <div className=" h-screen flex flex-col items-center justify-center">
+      <div className="bg-slate-200 p-8 rounded shadow-md text-center flex flex-col items-center w-[50%]">
+        <p className="font-bold text-3xl pb-10">Login Or Signup as Candidate or Admin</p>
+      <button onClick={() => handleClick("Candidate")} className="bg-slate-500 text-white py-2 px-4 rounded">Candidate</button><br />
+      <button onClick={() => handleClick("Admin")} className="bg-slate-500 text-white py-2 px-4 rounded">Admin</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
